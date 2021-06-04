822725eba042947fdb47b9c12ca5a18449a75102
asmatar <deruelle.arthur@gmail.com>
2021-06-04 23:04:57 +0200

    other way to update

--- src/App.js
+++ src/App.js
@@ -23,15 +23,26 @@ function App() {
 
   
   const changeCompleted = (id) => {
-    // we've got the task's id, so we want to stock it in a variable
-      const taskComplete = todos.findIndex(todo => todo.id === id)
-    // we copy todos to create a new todo list
-      const newTaskCompleted = [...todos]
-      // for the task that we want to change, we copie it data, but adding is completed
-      newTaskCompleted[taskComplete] ={
-        ...newTaskCompleted[taskComplete],
-        isCompleted: !newTaskCompleted[taskComplete].isCompleted
-    }
+    // METHODE 1
+        // we've got the task's id, so we want to stock it in a variable
+          const taskComplete = todos.findIndex(todo => todo.id === id)
+        // we copy todos to create a new todo list
+          const newTaskCompleted = [...todos]
+        // for the task that we want to change, we copie it data, but adding is completed
+            newTaskCompleted[taskComplete] ={
+              ...newTaskCompleted[taskComplete],
+              isCompleted: !newTaskCompleted[taskComplete].isCompleted
+            }
+      // FIN METHODE 1
+      // METHODE 2 !
+            // const newTaskCompleted = [...todos].map(todo => {
+            //   if (todo.id === id) {
+            //     todo.isCompleted = !todo.isCompleted
+            //   }
+            //   return todo
+            // })
+      // FIN METHODE 2
+        
     // we give the new task list to setTodos to change the state
     setTodos(newTaskCompleted)
   }
