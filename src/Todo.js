d3ea451a63e30b5dbd8f721a5848615f20fc5c77
asmatar <deruelle.arthur@gmail.com>
2021-06-04 11:03:35 +0200

    icon

--- src/Todo.js
+++ src/Todo.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { FaEdit, FaTrash } from "react-icons/fa";
 import './todo.scss';
-
 function Todo({todos}) {
     return (
         <div className='todo-container'>
             {
                 todos.map((todo) => {
-                    return <div key={todo.id} className='todo'> {todo.text}</div>
+                    return (
+                        <div key={todo.id} className='todo'> 
+                            {todo.text} 
+                            <div className='icons'>
+                                <span><FaTrash /></span>
+                                <span><FaEdit/></span>
+                            </div>
+                        </div>
+                    )
                 })
             }
        </div>
